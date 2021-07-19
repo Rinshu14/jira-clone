ee1bb69fb4cee587c3ed8d97312130e11e29e1e0
Rinshu14 <rajputrinshu@gmail.com>
2021-07-19 18:20:42 +0530

    commented

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//loadTasks();
+
 var uid = new ShortUniqueId();
 let body = document.querySelector("body");
 let addbtn = document.querySelector(".add");
@@ -6,20 +6,23 @@ let grid = document.querySelector(".grid");
 let delebtn = document.querySelector(".delete");
 let priority = ["pink", "green", "blue", "black"];
 let deleteMode = false;
-
+//if we do not have object to store our ticket create it by name AllTicket
 if (localStorage.getItem("AllTicket") == undefined) {
     let AllTicket = {};
 
     localStorage.setItem("AllTicket", JSON.stringify(AllTicket));
 }
-loadTasks();
+loadTasks();//it is a function that loads all the ticket whenever we start our app
+//delete ticket
 delebtn.addEventListener("click", function (e) {
 
     if (e.currentTarget.classList.contains("delete-select")) {
+        //if delete mode is already on then it off it by removing class delete select that is for deleting ticket
         e.currentTarget.classList.remove("delete-select");
         deleteMode = false;
     }
     else {
+          //if delete mode is off then it on it by adding class delete select
         e.currentTarget.classList.add("delete-select");
         deleteMode = true;
     }
@@ -27,8 +30,8 @@ delebtn.addEventListener("click", function (e) {
 
 })
 let colorfilters = document.querySelectorAll(".filter div");
-let filter = document.querySelectorAll(".filter");
-let filterMode = [false, false, false, false];
+let filter = document.querySelectorAll(".filter");//for fetching  filter in a array
+let filterMode = [false, false, false, false];//setting filter mode of each filter intially off it is just array created by us to check status of filters
 for (let i = 0; i < colorfilters.length; i++) {
     colorfilters[i].addEventListener("click", function (e) {
        
@@ -38,13 +41,16 @@ for (let i = 0; i < colorfilters.length; i++) {
             for (j = 0; j < filterMode.length; j++) {
                 filterMode[j] = false;
                 filter[j].classList.remove("filter-select");
-
+              //it sets all the filter mode to false if alredy selected and remove class filter select it just adds a backgournd color to filter
             }
+            //setting current filter background and fetching color to provide it to load task function 
+            //so that it can load the tickets of only that colour
             filterMode[i] = true;
             filter[i].classList.add("filter-select");
             filtercolor = e.currentTarget.classList[0];
         }
         else {
+            //to de select the filter at second click
             filter[i].classList.remove("filter-select");
             filterMode[i] = false;
 
@@ -60,8 +66,11 @@ addbtn.addEventListener("click", function () {
     delebtn.classList.remove("delete-select");
     deleteMode = false;
     if (existmodal != null) {
+        //if the ticket creating ui is there than return\
+
         return;
     }
+    //if not than create
     let div = document.createElement("div");
     div.innerHTML = `<div class="modal">
     <div class="text-section">
@@ -80,7 +89,7 @@ addbtn.addEventListener("click", function () {
 </div>`;
     body.append(div);
 
-    let ticketColor = "black";
+    let ticketColor = "black";//default priority color
     let allModalPriority = div.querySelectorAll(".modal-priority");
     for (let i = 0; i < allModalPriority.length; i++) {
         allModalPriority[i].addEventListener("click", function (e) {
@@ -104,21 +113,26 @@ addbtn.addEventListener("click", function () {
         <div class="actual-task" contenteditable="true">${taskinnercontainer.innerText}</div>`
 
         let ticketColorDiv = ticketdiv.querySelector(".ticket-color");
+        //to cahge color of ticket
         ticketColorDiv.addEventListener("click", function (e) {
             let currcolor = e.currentTarget.classList[1];
             let index = -1;
+            //loop to get current color
             for (let i = 0; i < priority.length; i++) {
                 if (priority[i] == currcolor) {
                     index = i;
 
                 }
             }
+             //feching the next colour of current color
             index++;
             index = index % 4;
             let newcolor = priority[index];
             ticketColorDiv.classList.remove(currcolor)
+            //adding new color class
             ticketColorDiv.classList.add(newcolor);
             let currid = ticketdiv.getAttribute("dataId");
+            //storing in local storage
             let allticket = JSON.parse(localStorage.getItem("AllTicket"));
             allticket[currid].color = newcolor;
             localStorage.setItem("AllTicket", JSON.stringify(allticket));
@@ -188,6 +202,7 @@ function loadTasks(color) {
         <div class="ticket-id">${currTicketId}</div>
         <div class="actual-task" contenteditable="true">${currTicketObj.taskValue}</div>`
         let ticketColorDiv = ticketdiv.querySelector(".ticket-color");
+        //adding event listner to change priority color
         ticketColorDiv.addEventListener("click", function (e) {
             let currcolor = e.currentTarget.classList[1];
             let index = -1;
@@ -209,6 +224,7 @@ function loadTasks(color) {
         })
         let actualTask = ticketdiv.querySelector(".actual-task");
         actualTask.setAttribute("dataId", currTicketId);
+        //to store changed task
         actualTask.addEventListener("input", function (e) {
             let updatedTask = e.currentTarget.innerText;
             let currid = e.currentTarget.getAttribute("dataId");
@@ -216,6 +232,7 @@ function loadTasks(color) {
             allticket[currid].task = updatedTask;
             localStorage.setItem("AllTicket", JSON.stringify(allticket));
         });
+        //to dlete ticket
         ticketdiv.addEventListener("click", function (e) {
             if (deleteMode) {
                 let currid = e.currentTarget.getAttribute("dataId");
